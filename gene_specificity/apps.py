24ad3ae8e49ddfd062f7a36b719ed64ed8b0ca71
veenhouse <Luke.J.Veenhuis@dartmouth.edu>
2022-06-06 10:03:52 -0400

    updating for testing

--- gene_specificity/apps.py
+++ gene_specificity/apps.py
@@ -5,8 +5,4 @@ from django.apps import AppConfig
 class GeneSpecificityConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gene_specificity'
-<<<<<<< HEAD
     label = 'gene_specificity'
-=======
-    label = 'gene_specificity'
->>>>>>> 92ca8779c719cb4d288fcb3cc377ddc02c407b0a
