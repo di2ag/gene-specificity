42422ca16b8ecbea8c0901d3554d09269f76e44c
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2023-05-28 16:34:16 -0400

    bug

--- gene_specificity/trapi_interface.py
+++ gene_specificity/trapi_interface.py
@@ -118,7 +118,7 @@ class TrapiInterface:
                     subjects = SpecificityMeanGene.objects.filter(gene_curie=curie).reverse()[:threshold]
                 else:
                     subjects = SpecificityMeanTissue.objects.filter(tissue_curie=curie).reverse()[:threshold]
-                if len(subjects) > 0
+                if len(subjects) > 0:
                     logger.info('Found results for {}'.format(curie))
                     subject_curies = [subject.get_result()[0] for subject in subjects]
                     vals = [subject.get_result()[2] for subject in subjects]
